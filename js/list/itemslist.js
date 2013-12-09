6ecad37ac4b63cd920deb58469c6a662621eabc4
sangramanand <sanand@indavest.com>
2013-12-09 16:43:19 +0530

    code changes to update the KOT screen for every 30 seconds (as suggested by SIBI)
    > Modified code to clear the KOT console and fetch KOT lines for every
    30 seconds.
    > Added Jquery plugin jquery.timer

--- js/list/itemslist.js
+++ js/list/itemslist.js
@@ -19,6 +19,7 @@ var ListSentOrderLinesWidget = DataTableWidget.extend({
             "bPaginate": false,
             "bLengthChange": false,
             "bFilter": true,
+			"bDestroy": true,
             "iDisplayLength": 15,
             "bSort": true,
             "bInfo": false,
@@ -54,30 +55,56 @@ var ListSentOrderLinesWidget = DataTableWidget.extend({
 			datum.itemStatus = status;
             return datum;
     },
-    _fetchSentOrderLines: function() {
-		var me = this;
-        $.ajax({
-			type: 'get',
-			url: this.url.restURL,
-			dataType: 'json',
-			async: false,
-			data:{
-				'Authorization' : 'Basic VG9pdEFkbWluOnRvaXRhZG1pbg=='
-			},
-			success: function(response) {
-				me.initView($.parseJSON(response.data));
-			},
-			error: function (response) {
-			}
+	_startTimer: function(){
+		var self = this;
+		
+		self.dom.timer = $.timer(30000, function() {
+		
+		self.widget.fnClearTable();
+		
+			$.ajax({
+				type: 'get',
+				url: self.url.restURL,
+				dataType: 'json',
+				async: false,
+				data:{
+					'Authorization' : 'Basic VG9pdEFkbWluOnRvaXRhZG1pbg=='
+				},
+				success: function(response) {
+					self.initView($.parseJSON(response.data));
+				},
+				error: function (response) {
+				}
+			});
 		});
+	},
+    _fetchSentOrderLines: function() {
+		var self = this;
+		$.ajax({
+				type: 'get',
+				url: this.url.restURL,
+				dataType: 'json',
+				async: false,
+				data:{
+					'Authorization' : 'Basic VG9pdEFkbWluOnRvaXRhZG1pbg=='
+				},
+				success: function(response) {
+					self.initView($.parseJSON(response.data));
+				},
+				error: function (response) {
+				}
+		});
+
     },
 	_changeKotItemStatus: function(itemId, itemStatus) {
 		var status;
 		if(itemStatus === 'SENT') {
 			status = 'INPROGRESS';
 		} else if(itemStatus === 'INPROGRESS') {
-			status = 'READY';
-		} else if(itemStatus === 'READY') {
+			status = 'PREPARED';
+		} else if(itemStatus === 'PREPARED') {
+			status = 'INTRANSIT';
+		} else {
 			return;
 		}
 		var requestParams = {
@@ -104,6 +131,9 @@ var ListSentOrderLinesWidget = DataTableWidget.extend({
         var self = this;
 		$("#organizations-list-table tbody tr").find('button').on('click', function(){
 			self._changeKotItemStatus(this.id, this.textContent);
+		});
+		self.dom.timerButton.on('click', function(){
+			self._startTimer();
 		});
         self._bindSearchEvents();
     }
